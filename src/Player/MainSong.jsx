0397c8fbc9042acb0544f18644830c2420ca0385
scrat98 <artemgolovko98@gmail.com>
2018-05-29 02:18:19 +0300

    add Next/Prev buttons

--- src/Player/MainSong.jsx
+++ src/Player/MainSong.jsx
@@ -1,14 +1,56 @@
 import React from 'react'
+import ClassNames from 'classnames'
 import { withCustomAudio } from 'react-soundplayer/addons'
 import {
   PlayButton,
   Timer,
   VolumeControl,
   Progress,
-  PrevButton,
-  NextButton,
-  Icons
+  Icons,
 } from 'react-soundplayer/components'
+
+
+const PrevButton = ({ className, style, onPrevClick }) => {
+  const { PrevIconSVG } = Icons
+  const classNames = ClassNames('sb-soundplayer-btn sb-soundplayer-prev-btn', className)
+
+  return (
+    <button type="button" className={classNames} style={style}
+            onClick={onPrevClick}>
+      <PrevIconSVG/>
+    </button>
+  )
+}
+
+const NextButton = ({ className, style, onNextClick }) => {
+  const { NextIconSVG } = Icons
+  const classNames = ClassNames('sb-soundplayer-btn sb-soundplayer-next-btn', className)
+
+  return (
+    <button type="button" className={classNames} style={style}
+            onClick={onNextClick}>
+      <NextIconSVG/>
+    </button>
+  )
+}
+
+const PreloaderIconSvg = (props) => (
+  <svg
+    className="mdl-js sb-soundplayer-icon"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 32 32"
+    width="32"
+    height="32"
+    fill="#FC561E">
+    <path opacity=".25"
+          d="M16 0 A16 16 0 0 0 16 32 A16 16 0 0 0 16 0 M16 4 A12 12 0 0 1 16 28 A12 12 0 0 1 16 4"/>
+    <path d="M16 0 A16 16 0 0 1 32 16 L28 16 A12 12 0 0 0 16 4z"
+          transform="rotate(8.0328 16 16)">
+      <animateTransform attributeName="transform" type="rotate" from="0 16 16"
+                        to="360 16 16" dur="0.8s" repeatCount="indefinite"/>
+    </path>
+  </svg>
+)
 
 class MainSong extends React.Component {
   render() {
@@ -23,6 +65,7 @@ class MainSong extends React.Component {
       trackTitle,
       volume,
     } = this.props
+
     return (
       <React.Fragment>
         <div className="flex-column items-center bg-darken-1 red rounded">
@@ -30,17 +73,15 @@ class MainSong extends React.Component {
             value={currentTime / duration * 100 || 0}
             soundCloudAudio={soundCloudAudio}/>
           <div className="flex items-center">
-            <PrevButton
-              soundCloudAudio={soundCloudAudio}/>
+            <PrevButton onPrevClick={this.props.prevSong}/>
             <PlayButton
               className="flex"
               playing={playing}
               seeking={seeking}
-              seekingIcon={Icons.SoundCloudLogoSVG()}
+              seekingIcon={<PreloaderIconSvg/>}
               soundCloudAudio={soundCloudAudio}
             />
-            <NextButton
-              soundCloudAudio={soundCloudAudio}/>
+            <NextButton onNextClick={this.props.nextSong}/>
             <VolumeControl
               className="flex items-center"
               rangeClassName="mb1"
--- src/Player/Song.jsx
+++ src/Player/Song.jsx
@@ -4,10 +4,10 @@ import styled from 'styled-components'
 
 const PlayButton = styled.button``;
 
-export default ({playing, play, trackTitle, date}) => (
+export default ({playing, loading, play, trackTitle, date}) => (
   <React.Fragment>
     <div
-      className="mb1 flex-column items-center bg-darken-1 red rounded">
+      className="mb1 p1 flex-column items-center bg-darken-1 red rounded">
       <div className="flex items-center">
         <PlayButton
           playing={playing}
