59815379bf164572fc6d7420530dbd19bb69ff18
scrat98 <artemgolovko98@gmail.com>
2018-05-29 02:27:50 +0300

    add Controls

--- src/Player/MainSong.jsx
+++ src/Player/MainSong.jsx
@@ -1,56 +1,12 @@
 import React from 'react'
-import ClassNames from 'classnames'
 import { withCustomAudio } from 'react-soundplayer/addons'
+import { PrevButton, NextButton, PreloaderIconSvg } from './Controls'
 import {
   PlayButton,
   Timer,
   VolumeControl,
   Progress,
-  Icons,
 } from 'react-soundplayer/components'
-
-
-const PrevButton = ({ className, style, onPrevClick }) => {
-  const { PrevIconSVG } = Icons
-  const classNames = ClassNames('sb-soundplayer-btn sb-soundplayer-prev-btn', className)
-
-  return (
-    <button type="button" className={classNames} style={style}
-            onClick={onPrevClick}>
-      <PrevIconSVG/>
-    </button>
-  )
-}
-
-const NextButton = ({ className, style, onNextClick }) => {
-  const { NextIconSVG } = Icons
-  const classNames = ClassNames('sb-soundplayer-btn sb-soundplayer-next-btn', className)
-
-  return (
-    <button type="button" className={classNames} style={style}
-            onClick={onNextClick}>
-      <NextIconSVG/>
-    </button>
-  )
-}
-
-const PreloaderIconSvg = (props) => (
-  <svg
-    className="mdl-js sb-soundplayer-icon"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 32 32"
-    width="32"
-    height="32"
-    fill="#FC561E">
-    <path opacity=".25"
-          d="M16 0 A16 16 0 0 0 16 32 A16 16 0 0 0 16 0 M16 4 A12 12 0 0 1 16 28 A12 12 0 0 1 16 4"/>
-    <path d="M16 0 A16 16 0 0 1 32 16 L28 16 A12 12 0 0 0 16 4z"
-          transform="rotate(8.0328 16 16)">
-      <animateTransform attributeName="transform" type="rotate" from="0 16 16"
-                        to="360 16 16" dur="0.8s" repeatCount="indefinite"/>
-    </path>
-  </svg>
-)
 
 class MainSong extends React.Component {
   render() {
